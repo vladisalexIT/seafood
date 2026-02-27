a7c71ec725cbeac31c18859278d5433a2daf0b6f
vladisalex-it <vl4disalex@gmail.com>
2026-02-27 23:57:34 +0300

    feat: add product catalog section with vertical slider and detailed seafood descriptions

--- js/main.js
+++ js/main.js
@@ -8,4 +8,28 @@ $(function () {
         prevArrow: '<button type="button" class="slick-prev slider-arrow"><img src="./icons/slider-arrow-top.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next slider-arrow"><img src="./icons/slider-arrow-down.svg" alt=""></button>',
     })
+    
+
+    $('.product__name').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        focusOnSelect: true,
+        asNavFor: '.product__content',
+        vertical: true,
+        prevArrow: '<button type="button" class="product-prev"><img src="./icons/product-arrow-up.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="product-next"><img src="./icons/product-arrow-down.svg" alt=""></button>',
+        onInit: function (slick) {
+            slick.$slides.eq(0).addClass('slick-current');
+        }
+    });
+    $('.product__content').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.product__name',
+        vertical: true,
+        arrows: false,
+        
+    });
 })
+
+
