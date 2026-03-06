01079e2e05584eab7d63ad5f47978118ad654a67
vladisalex-it <vl4disalex@gmail.com>
2026-03-06 09:33:51 +0300

    responsive(product section): adapt layout for mobile and tablet devices

--- js/main.js
+++ js/main.js
@@ -21,7 +21,41 @@ $(function () {
         nextArrow: '<button type="button" class="product-next"><img src="./icons/product-arrow-down.svg" alt=""></button>',
         onInit: function (slick) {
             slick.$slides.eq(0).addClass('slick-current');
-        }
+        },
+        responsive: [
+            {
+                breakpoint: 916, // ← Каждый объект отдельно!
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    vertical: false,
+                    arrows: false,
+                    centerMode: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 631, // ← Отдельный объект!
+                settings: {
+                    slidesToShow: 2,
+                    vertical: false,
+                    arrows: false,
+                    dots: true,
+                    centerMode: false,
+                },
+            },
+            {
+                breakpoint: 471, // ← Отдельный объект!
+                settings: {
+                    slidesToShow: 1,
+                    vertical: false,
+                    arrows: false,
+                    dots: true,
+                    centerMode: false,
+                },
+            },
+        ],
     });
     $('.product__content').slick({
         slidesToShow: 1,
@@ -29,14 +63,14 @@ $(function () {
         asNavFor: '.product__name',
         vertical: true,
         arrows: false,
-
+        centerMode: false, // Важно! Выключаем центрирование
+        centerPadding: '0', // Отключаем внутренние отступы
+        variableWidth: false, // Фиксированная ширина слайдов
     });
 
 
-    $('.menu__list-burger-button').on('click', function() {
-        // Переключаем класс для выезда меню
+    $('.menu__list-burger-button').on('click', function () {
         $('.menu__list').toggleClass('menu__list--active');
-        // Переключаем класс для анимации кнопки в крестик
         $(this).toggleClass('menu__list-burger-button--active');
     });
 })
